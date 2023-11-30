c7dc017e6defc1679e367ae140157131a261d227
CJ-Presley <144433536+CJ-Presley@users.noreply.github.com>
2023-11-30 13:22:25 +0000

    commit - going for careers interview

---
+++ charles-ka2/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+
+function CookieNav() {
+  return (
+    <>
+      <Navbar className="fixed-top" bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="/">
+            <b>Biscuit Clicker</b>
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/biscuit">Biscuit Clicker</Nav.Link>
+            <Nav.Link href="/temp">Temp Check</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+export default CookieNav;
---
+++ charles-ka2/src/constants/paths.ts
@@ -0,0 +1,6 @@
+const INDEX_PATH = "/";
+const CLICKER_PATH = "/biscuit";
+const TEMP_PATH = "/temp";
+const NOT_FOUND_PATH = "*";
+
+export { INDEX_PATH, CLICKER_PATH, TEMP_PATH, NOT_FOUND_PATH };
